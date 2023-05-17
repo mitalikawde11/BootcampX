d71f35a42c9e45e693aeb2cc0a42d7b2d6da935e
Mitali <mitali.kawde11@gmail.com>
2023-05-17 10:21:46 -0700

    Getting currently enrolled students average assignment completion time

---
+++ 3_queries_group-by/5_average_completion_time.sql
@@ -0,0 +1,10 @@
+-- Get currently enrolled students' average assignment completion time.
+-- Order the results from greatest duration to least greatest duration.
+-- A student will have a null end_date if they are currently enrolled
+
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+WHERE end_date IS NULL
+GROUP BY student
+ORDER BY average_assignment_duration DESC;
